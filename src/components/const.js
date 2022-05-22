0cf8804ba00c8d71e253fa8fcc6ab9cd6edaea7e
Janvinsha <janvinsha@gmail.com>
2022-05-22 21:16:28 +0100

    Refactor code

--- src/components/const.js
+++
@@ -1,9 +0,0 @@
-
-export const CONTRACT_ADDRESS =
-  process.env.WLD_CONTRACT_ADDRESS || // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing
-  "0x32D59776E91fdb3F377755e12cEC05d9067c2B4F";
-
-export const CONTRACT_ABI = [
-  // Function to claim the airdrop
-  "function claim (address receiver, uint256 root, uint256 nullifierHash, uint256[8] calldata proof)",
-];
--- src/index.js
+++ src/index.js
@@ -5,77 +5,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
-import * as Sentry from '@sentry/react';
-import _ from 'underscore';
-import { ethers } from 'ethers';
-import BN from 'bignumber.js';
 import client from './apollo';
 
-import { Integrations } from '@sentry/tracing';
-// import after NETWORK_CONFIGs is initialized
-import Wallet from './utils/wallet';
-import TokenListManager from './utils/tokenList';
-import GlobalStateManager from './utils/global';
-import SwapFn from './utils/swapFn';
-// import Nxtp from './utils/nxtp';
-import HopUtils from './utils/hop';
-import TxQueue from './utils/txQueue';
 // import Storage from './utils/storage';
-
-const IS_MAIN_NETWORK = (window.IS_MAIN_NETWORK =
-  process.env.IS_MAIN_NETWORK === 'true');
-window.IS_PRODUCTION = process.env.IS_PRODUCTION;
-
-if (process.env.IS_PRODUCTION) {
-  Sentry.init({
-    dsn: process.env.SENTRY_JS_DSN,
-    environment: IS_MAIN_NETWORK ? 'production' : 'development',
-    integrations: [new Integrations.BrowserTracing()],
-    release: `${process.env.HEROKU_APP_NAME}-${process.env.HEROKU_RELEASE_VERSION}`,
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
-}
-
-window.ethers = ethers;
-window._ = _;
-window.BN = BN;
-window.BigNumber = ethers.BigNumber;
-
-if (IS_MAIN_NETWORK) {
-  console.log('Loading MAIN config...');
-} else {
-  console.log('Loading TEST config...');
-}
-
-// const config =  fetch(
-//   IS_MAIN_NETWORK ? '/config/main.config.json' : '/config/test.config.json',
-// );
-// window.NETWORK_CONFIGS = await config.json();
-window.MAX_RETRIES = process.env.IS_PRODUCTION ? 3 : 1;
-
-// pre-load and collase/parallelize all our external JSON config loading
-// to reduce initial app load times
-// Promise.all([Wallet.initializeAbis(), TokenListManager.initializeTokenLists()]);
-
-// Storage.initialize();
-// TokenListManager.initialize();
-// TokenListManager.updateTokenList();
-// GlobalStateManager.initialize();
-// Wallet.initialize();
-
-// SwapFn.initialize();
-//  Nxtp.initalize();
-// TxQueue.initialize();
-
-if (Wallet.isMetamaskSupported()) {
-  console.log('MetaMask is installed!');
-} else {
-  console.error('Metamask not installed!');
-}
 
 ReactDOM.render(
   <Router>
